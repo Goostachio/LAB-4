2bbf342ad2ab99b6dfbfb6c82eea3a752a4330fc
Đào Hồng Minh <135724738+Goostachio@users.noreply.github.com>
2025-04-08 15:12:20 +0700

    reverse if condition  lab4_main.ino

--- lab4_main.ino
+++ lab4_main.ino
@@ -1,78 +1,76 @@
 #include <scheduler.h>
 #include <software_timer.h>
-
 #define LED_PIN 48
+#define D3 6
+#define D4 7
+#define D5 8
+#define D6 9
 
-#define INIT        0
-#define RED_GREEN   1
-#define RED_AMBER   2
-#define GREEN_RED   3
-#define AMBER_RED   4
+#define INIT 0
+#define RED_GREEN  1
+#define RED_AMBER  2
+#define GREEN_RED  3
+#define AMBER_RED  4
 
 int led_status = 0;
-int state = RED_GREEN;
 
-void blinky() {
-  if (led_status == 0) {
-    digitalWrite(D3, HIGH);
-    digitalWrite(D4, HIGH);
-  } else {
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, HIGH);
-  }
-  led_status = 1 - led_status;
+void blinky(){
+	if(led_status == 0){
+      digitalWrite(6, HIGH);digitalWrite(7, HIGH);
+    }else{
+      digitalWrite(6, LOW);digitalWrite(7, HIGH);
+    }
+    led_status = 1 - led_status;
 }
 
-void trafficLight() {
+int state = RED_GREEN;
+
+void trafficLight(){
   switch (state) {
     case RED_GREEN:
-      if (!isTimerExpired(0)) break;
-      setTimer(0, 300);
-      state = RED_AMBER;
+      if (isTimerExpired(0) == 1) {
+        setTimer(0, 300);
+        state = RED_AMBER;
+      }
 
-      digitalWrite(D3, HIGH);
-      digitalWrite(D4, HIGH);
-      digitalWrite(D5, HIGH);
-      digitalWrite(D6, LOW);
+      digitalWrite(6, HIGH);digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);digitalWrite(9, LOW);
       break;
 
     case RED_AMBER:
-      if (!isTimerExpired(0)) break;
-      setTimer(0, 200);
-      state = GREEN_RED;
+      if (isTimerExpired(0) == 1) {
+        setTimer(0, 200);
+        state = GREEN_RED;
+      }
+      digitalWrite(6, HIGH);digitalWrite(7, HIGH);
+      digitalWrite(8, LOW);digitalWrite(9, HIGH);
 
-      digitalWrite(D3, HIGH);
-      digitalWrite(D4, HIGH);
-      digitalWrite(D5, LOW);
-      digitalWrite(D6, HIGH);
       break;
 
     case GREEN_RED:
-      if (!isTimerExpired(0)) break;
-      setTimer(0, 300);
-      state = AMBER_RED;
-
-      digitalWrite(D3, HIGH);
-      digitalWrite(D4, LOW);
-      digitalWrite(D5, HIGH);
-      digitalWrite(D6, HIGH);
+      if (isTimerExpired(0) == 1) {
+        setTimer(0, 300);
+        state = AMBER_RED;
+      }
+      digitalWrite(6, HIGH);digitalWrite(7, LOW);
+      digitalWrite(8, HIGH);digitalWrite(9, HIGH);
+      
       break;
 
     case AMBER_RED:
-      if (!isTimerExpired(0)) break;
-      setTimer(0, 200);
-      state = RED_GREEN;
-
-      digitalWrite(D3, LOW);
-      digitalWrite(D4, HIGH);
-      digitalWrite(D5, HIGH);
-      digitalWrite(D6, HIGH);
+      if (isTimerExpired(0) != 1) {
+        setTimer(0, 200);
+        state = RED_GREEN;
+        }
+      digitalWrite(6, LOW);digitalWrite(7, HIGH);
+      digitalWrite(8, HIGH);digitalWrite(9, HIGH);
+      
       break;
   }
 }
 
 void TIMER_ISR(void *pvParameters) {
-  while (1) {
+  while(1) {
     SCH_Update();
     vTaskDelay(10);
   }
@@ -80,11 +78,12 @@ void TIMER_ISR(void *pvParameters) {
 
 void setup() {
   xTaskCreate(TIMER_ISR, "TIMER_ISR", 2048, NULL, 2, NULL);
-
+  
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
+
   pinMode(LED_PIN, OUTPUT);
 
   SCH_Init();
